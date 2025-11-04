969994342a32c9e745888c8cce98c014910e2485
mason <mkaydo@bsd220.org>
2025-11-04 00:47:14 -0600

    Initial Commit

---
+++ tictactoe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tictactoe
+//
+//  Created by mason kaydo on 11/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by mason kaydo on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
